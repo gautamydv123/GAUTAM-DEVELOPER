a93cdf8c1778a0c72bbba91ee5144dd30f3e58dc
gautamydv123 <gautammotlakalan@gmail.com>
2024-09-26 11:33:25 +0530

    projects

---
+++ salary slip/index.js
@@ -0,0 +1,93 @@
+const button=document.getElementById('btn1');
+button.addEventListener('click',()=>{
+ let inputname=document.getElementById('in_name').value
+ const outputname=document.getElementById('out_name');
+ outputname.innerText=inputname;
+
+ let inputcode=document.getElementById('in_bankcode').value
+ const outputcode=document.getElementById('out_bankcode')
+ outputcode.innerText=inputcode
+ 
+ let inputbranch=document.getElementById('in_branchname').value
+ const outputbranch=document.getElementById('out_branch')
+ outputbranch.innerText=inputbranch
+
+ let inputemp=document.getElementById('in_empcode').value
+ const outputemp=document.getElementById('out_empcode')
+ outputemp.innerText=inputemp
+
+ let inputbankname=document.getElementById('in_bankname').value
+ const outputbankname=document.getElementById('out_bankname')
+ outputbankname.innerText=inputbankname
+
+ let inputpayslip=document.getElementById('in_payslipno').value
+ const outpayslip=document.getElementById('out_payslip')
+ outpayslip.innerText=inputpayslip
+
+ let inputcostcenter=document.getElementById('in_costcenter').value
+ const outputcostcenter=document.getElementById('out_costcenter')
+ outputcostcenter.innerText=inputcostcenter
+
+ let inputbankbarnch=document.getElementById('in_bankbranch').value
+ const outputbankbranch=document.getElementById('out_bankbranch')
+ outputbankbranch.innerText=inputbankbarnch
+
+ let inputpayperiod=document.getElementById('in_payperiod').value
+ const outputpayperiod=document.getElementById('out_payperiod')
+ outputpayperiod.innerText=inputpayperiod
+
+ let inputdescription=document.getElementById('in_ccdescription').value
+ const outputdescription=document.getElementById('out_description')
+ outputdescription.innerText=inputdescription
+
+ let inputacno=document.getElementById('in_bankac').value
+ const outputacno=document.getElementById('out_acno')
+ outputacno.innerText=inputacno
+
+ let inputpersonal=document.getElementById('in_personalarea').value
+ const outputpersonal=document.getElementById('out_personal')
+ outputpersonal.innerText=inputpersonal
+
+ let inputgrade=document.getElementById('in_grade').value
+ const outputgrade=document.getElementById('out_grade')
+ outputgrade.innerText=inputgrade
+
+ let inputempgroup=document.getElementById('in_empgroup').value
+ const outputempgroup=document.getElementById('out_empgroup')
+ outputempgroup.innerText=inputempgroup
+
+ let inputphno=document.getElementById('in_panno').value
+ const outputphno=document.getElementById('out_phno')
+ outputphno.innerText=inputphno
+
+ let inputbasic=document.getElementById('in_basicsalary').value
+ const outbasic=document.getElementById('out_basicsalary')
+ outbasic.innerText=inputbasic
+
+
+ let inputfund=document.getElementById('in_fund').value
+ const outputfund=document.getElementById('out_fund')
+ outputfund.innerText=inputfund
+
+ let inputfixed=document.getElementById('in_fixedallowence').value
+ const outputfixed=document.getElementById('out_fixed')
+ outputfixed.innerText=inputfixed
+
+ let inputlic=document.getElementById('in_lic').value
+ const outputlic=document.getElementById('out_lic')
+ outputlic.innerText=inputlic
+
+ let inputvariable=document.getElementById('in_variableallownece').value
+ const outputvariable=document.getElementById('out_variable')
+ outputvariable.innerText=inputvariable
+
+ let inputloan=document.getElementById('in_laon').value
+ const ouptuloan=document.getElementById('out_laon')
+ ouptuloan.innerText=inputloan
+
+
+ let inputrent=document.getElementById('in_houserent').value
+ const  outputrent=document.getElementById('out_housernet')
+ outputrent.innerText=inputrent
+
+}) 
